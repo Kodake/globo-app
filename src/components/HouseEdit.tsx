66bc70f4865dd3e1f9afe6806e25b1d04c62c235
Kodake <kodake@hotmail.es>
2022-08-23 16:56:22 -0400

    Adding validation summary for the client side

--- src/components/HouseEdit.tsx
+++ src/components/HouseEdit.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom';
 import ApiStatus from '../api/ApiStatus';
 import { useFetchHouse, useUpdateHouse } from '../hooks/useHouses';
+import ValidationSummary from '../validations/ValidationSummary';
 import HouseForm from './HouseForm';
 
 const HouseEdit = () => {
@@ -13,16 +14,21 @@ const HouseEdit = () => {
     const houseId = parseInt(id);
 
     const { data, status, isSuccess } = useFetchHouse(houseId);
-    const updateHoue = useUpdateHouse();
+    const updateHouse = useUpdateHouse();
 
     if (!isSuccess)
         return <ApiStatus status={status} />
 
     return (
-        <HouseForm
-            house={data}
-            submitted={h => updateHoue.mutate(h)}
-        />
+        <>
+            {updateHouse.isError && (
+                <ValidationSummary error={updateHouse.error} />
+            )}
+            <HouseForm
+                house={data}
+                submitted={h => updateHouse.mutate(h)}
+            />
+        </>
     )
 }
 
