425bb08dbe89b03fadcef76acde00479b62329bf
Kodake <kodake@hotmail.es>
2022-08-25 00:52:21 -0400

    Added back to HouseList from img logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
 import CardHeader from 'react-bootstrap/esm/CardHeader';
+import { Link } from 'react-router-dom';
+import useFetchUser from '../hooks/useUsers';
 import logo from '../logo.svg';
 
 type Props = {
@@ -6,10 +8,17 @@ type Props = {
 }
 
 const Header = ({ subtitle }: Props) => {
+    const { data: userClaims } = useFetchUser();
+    
     return (
         <CardHeader className="row mb-4">
             <div className="col-5">
-                <img src={logo} alt="logo" className="logo" />
+                {userClaims &&
+                    userClaims.find((c) => c.type === "role" && c.value === "Admin") && (
+                        <Link to='/'>
+                            <img src={logo} alt="logo" className="logo" />
+                        </Link>
+                    )}
             </div>
             <div className="col-7 mt-5 subtitle">{subtitle}</div>
         </CardHeader>
